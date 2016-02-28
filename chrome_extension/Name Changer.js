e688d01bf2cc30c1decfca285601e94b84bf5fcc
Victor Williams <vrwilliams719@gmail.com>
2016-02-28 10:38:15 -0500

    names?

---
+++ chrome_extension/Name Changer.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    $('a').each(function(){
+      $('[Jeb Bush]').replace("Jeb Bush", "Please Clap");
+
+
+    });
+  });
+});
--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -2,5 +2,6 @@ chrome.browserAction.onClicked.addListener(function(tab){
 	console.log("GOP Candidates")
 	chrome.tabs.executeScript(null, {
 		file: "GOP.js"
+		file: "Name Changer.js"
 	});
 });
